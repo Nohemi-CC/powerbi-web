3978eb4e672dc9eba690c8d1b1fdfdd5deb41c38
Nohemi <nomy061004@gmail.com>
2025-06-06 17:08:37 -0600

    Logro:Mi primer commit

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,7 @@
 <template>
   <div class="container">
         <h1>Reporte de Power BI</h1>
-        <p>Visualización interactiva de
-            datos generada en Power BI.</p>
+         <p>Visualización interactiva de datos generada en Power BI desarrollada por Nohemi Campech Colin </p>
         <div class="iframe-container">
 
             <iframe title="VisualizaciónDatamartNotrwind" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiODhhMjBmZTUtOTIwOS00MTYxLTgyZDQtNTJkNGE1YzQ0NmM4IiwidCI6ImExY2FiMDNmLWIxZTgtNDk3Ny1iNGRjLTIzNDBjMTQwNzYzYyJ9" frameborder="0" allowFullScreen="true"></iframe>
