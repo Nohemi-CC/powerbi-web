e5a79877849324dd27c036e759d7f93de56f26bd
Nohemi <nomy061004@gmail.com>
2025-06-06 16:41:39 -0600

    Logro:Primer BI

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+        <h1>Reporte de Power BI</h1>
+        <p>Visualización interactiva de
+            datos generada en Power BI.</p>
+        <div class="iframe-container">
+
+            <iframe title="VisualizaciónDatamartNotrwind" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiODhhMjBmZTUtOTIwOS00MTYxLTgyZDQtNTJkNGE1YzQ0NmM4IiwidCI6ImExY2FiMDNmLWIxZTgtNDk3Ny1iNGRjLTIzNDBjMTQwNzYzYyJ9" frameborder="0" allowFullScreen="true"></iframe>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
